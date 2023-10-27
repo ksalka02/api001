31402cd5d910a6de55433ae076cdead3db7467bf
khalid salka <ksalka02@gmail.com>
2023-10-26 22:40:00 -0500

    update

--- mongo/maindb.py
+++ mongo/maindb.py
@@ -6,12 +6,17 @@ from pymongo import MongoClient
 import boto3
 
 # password = os.environ.get("MONGODB_PWD")
+# session = boto3.Session(profile_name='main')
+
 client = boto3.client('ssm', region_name='us-east-1')
 
 password = client.get_parameter(
     Name='/api/mongo/password',
     WithDecryption=True
 )
+pwd = password["Parameter"]["Value"]
 
-connection_string = f"mongodb+srv://ksalka:{password}@cluster0.wby46ms.mongodb.net/?retryWrites=true&w=majority"
+connection_string = f"mongodb+srv://ksalka:{pwd}@cluster0.wby46ms.mongodb.net/?retryWrites=true&w=majority"
+
+
 client = MongoClient(connection_string)
