d90043bf59c7d96bb2fb245a65adc4f9b9e2a06f
khalid salka <ksalka02@gmail.com>
2023-11-09 14:47:08 -0600

    comment out http provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.17.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.4.0"
-    }
+    # http = {
+    #   source  = "hashicorp/http"
+    #   version = "3.4.0"
+    # }
   }
 }
 
