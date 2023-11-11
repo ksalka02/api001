95778b7ecddef9d8724838e1732b64ec6f34d84f
khalid salka <ksalka02@gmail.com>
2023-11-10 22:50:09 -0600

    update

--- github/gittest.py
+++ github/gittest.py
@@ -1,17 +1,17 @@
-import json
-import requests
+# import json
+# import requests
 
-GITHUB_KEY = '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'
+# GITHUB_KEY = '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'
 
-PAYLOAD = {
-    'name': 'api_test1',
-    'public': 'true'
-}
+# PAYLOAD = {
+#     'name': 'api_test1',
+#     'public': 'true'
+# }
 
-res = requests.get(
-    'https://api.github.com/user/repos',
-    headers={'Authorization': f'token {GITHUB_KEY}'},
-    data=json.dumps(PAYLOAD)
-)
+# res = requests.get(
+#     'https://api.github.com/user/repos',
+#     headers={'Authorization': f'token {GITHUB_KEY}'},
+#     data=json.dumps(PAYLOAD)
+# )
 
-print(res.json()[0]['url'])
+# print(res.json()[0]['url'])
